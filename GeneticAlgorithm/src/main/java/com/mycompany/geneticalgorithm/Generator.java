6e17727144d73cf20f7caa0ea18029808efc5831
Swagath_Sai_001417567 <kalpatiananthakris.s@husky.neu.edu>
2019-04-07 16:47:54 -0400

    Added genearator class

---
+++ GeneticAlgorithm/src/main/java/com/mycompany/geneticalgorithm/Generator.java
@@ -0,0 +1,33 @@
+package com.mycompany.geneticalgorithm;
+
+public class Generator {
+
+    public char checkSex(String personId){
+
+        return '0';
+    }
+
+
+    public boolean checkFertilty(String personId){
+
+        return true;
+    }
+
+
+    public long generatePhenotype(Person person){
+
+
+
+        return 0;
+    }
+
+
+    public boolean isFit(){
+
+
+        return false;
+    }
+
+
+
+}
