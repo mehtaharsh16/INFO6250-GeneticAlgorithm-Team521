e54832fa1bdd078fe78788bb8f3a4d6539538a6d
ajaymohandas89 <mohandas.aj@husky.neu.edu>
2019-04-16 14:52:03 -0400

    Created GenoTypeTest file

---
+++ GeneticAlgorithm/src/test/java/com/mycompany/GeneticAlgorithmTest/GenotypeTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.GeneticAlgorithmTest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Welcome
+ */
+public class GenotypeTest {
+
+/*
+    @Test
+    public void test1() throws Exception{
+        
+    }
+*/
+
+  
+
+}
